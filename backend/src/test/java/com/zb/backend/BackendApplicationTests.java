14557da3b332bf6e2b6eba5df13cf5f50b7d8dee
Sofa-test <sotocoxufa7@hotmail.com>
2025-07-24 21:07:10 +0800

    初始化项目

---
+++ backend/src/test/java/com/zb/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zb.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
